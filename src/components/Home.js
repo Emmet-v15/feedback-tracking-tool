6214befdda340549c614f7b54ee46ccde82f5125
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2025-02-28 23:42:56 +0000

    Basic description added to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styles/Home.css'; // Import the Home CSS
+
 
 function Home() {
     return (
@@ -7,9 +9,27 @@ function Home() {
                 <h2>Home</h2>
             </section>
 
-            <section className="content-box">
-                <p>This is the home section of the website.</p>
+            <section className="title">
+                <h3>How it works</h3>
             </section>
+            
+            <section className="description">
+                <p>You create feedback about problems around campus. These problems are then seen and responded too by a member of staff as soon as possible. Hopefully getting your problem resolved.</p>
+            </section>
+
+            <section className="title">
+                <h3>How to create feedback</h3>
+            </section>
+
+            <section className="description">
+                <p>You can create feedback by first clicking on the "feedback" button in the navigation bar at the top of the page.</p>
+                <p>Once on that page you are able too create feedback using the green button in the top right hand corner. Once you have submitted feedback, a member of staff will respond to is as soon as possible</p>
+                <p>You can track how long your feedback has been open with the timer on the right hand side of your feedback</p>
+            </section>
+
+            <section className="gap">
+            </section>
+            
         </main>
     );
 }
