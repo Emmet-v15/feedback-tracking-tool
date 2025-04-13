14f4c683befda1416d5f3aa7f696e02b30663083
Emmet <emmet@v15.studio>
2025-04-13 05:50:56 +0100

    Moved towards bun, vite & dotenvx

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -59,7 +59,7 @@ app.get('/api/feedback', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backend is running on port ${PORT}`);
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': process.env.BACKEND_URL
+      '/api': process.env.VITE_API_URL || 'http://localhost:4000',
     },
-    port: process.env.APP_PORT,
+    
+    port: process.env.VITE_APP_PORT || 3000,
   }
 })
